967f3fdef7050a1bef3d56be8a01e73a52192893
JidexBD <ewuolajide7@gmail.com>
2025-12-28 03:45:27 +0100

    side bar done

--- components/comp_js/navigation-bar.js
+++
@@ -1,6 +0,0 @@
-fetch("/components/comp_html/navigation-bar.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("mynav-bar").innerHTML = data;
-  })
-  .catch((err) => console.error("Failed to load navbar:", err));
